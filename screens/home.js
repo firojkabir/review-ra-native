9db43739e205857cc077f4b127036be1d7aab084
firojkabir <firoj.kabir65@gmail.com>
2022-10-04 17:17:01 +0200

    Passing data between screens

--- screens/home.js
+++ screens/home.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import { Button, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, Text, TouchableOpacity, View } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 function Home({navigation}) {
-	const pressHandler = () => {
-		navigation.navigate('ReviewDetails')
-	}
+	const [reviews, setReviews] = useState([
+		{title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1'},
+		{title: 'Gotta Catch them All', rating: 4.3, body: 'lorem ipsum', key: '2'},
+		{title: 'Not So Final Fantasy', rating: 4.8, body: 'lorem ipsum', key: '3'},
+	])
 
   return (
 	<View style={globalStyles.containter}>
-		<Text style={globalStyles.titleText}>Home screen</Text>
-		<Button title='go to review dets' onPress={pressHandler}/>
+		<FlatList
+			data={reviews}
+			renderItem={({item}) => (
+				<TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+					<Text style={globalStyles.titleText}>
+						{item.title}
+					</Text>
+				</TouchableOpacity>
+			)}
+
+		 />
 	</View>
   )
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-function ReviewDetails() {
+function ReviewDetails({navigation}) {
   return (
 	<View style={globalStyles.containter}>
-		<Text style={globalStyles.titleText}> ReviewDetails screen</Text>
+		<Text style={globalStyles.titleText}>{navigation.getParam('title')}</Text>
+		<Text style={globalStyles.titleText}>{navigation.getParam('body')}</Text>
+		<Text style={globalStyles.titleText}>{navigation.getParam('rating')}</Text>
 	</View>
   )
 }
